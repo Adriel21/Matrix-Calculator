dc8dab2ed9d611a629161fd53a60c16a762511ef
Adriel21 <araujo.contato021@gmail.com>
2017-06-22 00:41:51 -0300

    Update BIBLIOTECA.h

--- BIBLIOTECA.h
+++ BIBLIOTECA.h
@@ -15,9 +15,43 @@ struct Vetor leituraDeVet (int elem){
     for(i=0; i<vetor.elem; i++){
     	printf("Elemento %d: ", i+1);
     	scanf("%d", &vetor.ender[i]);
-    	
-	printf("\n\n");
 	
 	}
+	
+	printf("\n\n");
 	return vetor;
-}				
+}			
+
+void exibicaoDeVet (struct Vetor vetor){
+	int i;
+	printf("(");
+	for(i=0; i<vetor.elem; i++){
+		if(i != vetor.elem-1){
+			printf("%d,", vetor.ender[i]);
+		}
+		else{
+			printf("%d)", vetor.ender[i]);
+		}
+	}
+}
+
+struct Vetor somaDeVet (struct Vetor vetorA , struct Vetor vetorB){
+	int i;
+	struct Vetor vetorRes;
+	vetorRes.ender= (int *) malloc(vetorA.elem*sizeof(int));
+	for(i=0; i< vetorA.elem; i++){
+		vetorRes.ender[i]= vetorA.ender[i] + vetorB.ender[i];
+	}
+	return vetorRes;
+}
+
+struct Vetor produtoPorEscalar (struct Vetor vetor, int multiplicador){
+	int i;
+	struct Vetor vetorRes;
+	vetorRes.elem=vetor.elem;
+	vetorRes.ender= (int *) malloc(vetor.elem*sizeof(int));
+	for(i=0; i< vetor.elem; i++){
+		vetorRes.ender[i]= multiplicador*vetor.ender[i];
+	}
+	return vetorRes;
+}
